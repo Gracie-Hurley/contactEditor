23a33f01162aeae69ace8adcf2d9600124e01614
2-GHurley <2-GHurley@BH-R20-32.local.gfmat.org>
2023-04-28 10:52:33 +0100

    28/03/2023, this program is to connect SQL to netbeans first commit of contact editor project

--- src/main/java/my/contacteditor/contactEditorUI.java
+++ src/main/java/my/contacteditor/contactEditorUI.java
@@ -3,17 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package my.contacteditor;
 
 /**
  *
  * @author 2-GHurley
  */
-public class NewJFrame extends javax.swing.JFrame {
+public class contactEditorUI extends javax.swing.JFrame {
 
     /**
-     * Creates new form NewJFrame
+     * Creates new form contactEditorUI
      */
-    public NewJFrame() {
+    public contactEditorUI() {
         initComponents();
     }
 
@@ -36,7 +37,7 @@ public class NewJFrame extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 291, Short.MAX_VALUE)
+            .addGap(0, 300, Short.MAX_VALUE)
         );
 
         pack();
@@ -59,20 +60,20 @@ public class NewJFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(contactEditorUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(contactEditorUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(contactEditorUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(contactEditorUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame().setVisible(true);
+                new contactEditorUI().setVisible(true);
             }
         });
     }
